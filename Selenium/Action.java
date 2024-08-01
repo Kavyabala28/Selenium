13c8607763ae758d7d38e6d6939142eb3a8e573f
Kavya B <161735556+Kavyabala28@users.noreply.github.com>
2024-08-01 19:14:48 +0530

    Add files via upload

--- Selenium/Action.java
+++ Selenium/Action.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.interactions.Actions;
 
 public class Action {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		ChromeDriver driver = new ChromeDriver();
 
@@ -26,23 +26,35 @@ public class Action {
 
 		Actions action = new Actions(driver);
 
-		// Drag And Drop By method
+		// Drag And Drop the box using 'dragAndDropBy' method
 		action.dragAndDropBy(dragAndDropBox, 200, 0).dragAndDropBy(dragAndDropBox, 200, 0).perform();
 
-		// Drag And Drop method
+		// Drag the box and drop in using 'dragAndDrop' method
 		WebElement source = driver.findElement(By.id("form:drag"));
 		WebElement destination = driver.findElement(By.id("form:drop"));
 		action.dragAndDrop(source, destination).perform();
 
-		// Scrolling to last using scrollToElement method
+		// Scrolling to last using 'scrollToElement' method
 		WebElement scroll = driver.findElement(By.xpath("//div[@class='card']/h4[text()='Range Slider']"));
 		action.scrollToElement(scroll).perform();
 
-		// Right click using contextClick method
+		// Right click using 'contextClick' method
 		WebElement rightClick = driver.findElement(By.xpath("//div[@class='card']/h5[text()='Progress Bar']"));
 		action.contextClick(rightClick).perform();
+		rightClick.click();
+
+		Thread.sleep(2000);
+
+		// Resize the image using 'clickAndHold' & 'moveByOffset' methods
+		WebElement img = driver.findElement(By.className("ui-wrapper"));
+		action.clickAndHold(img).moveByOffset(100, 50).perform();
+
+		// moving the cursor using 'moveToElement' method
+		WebElement move = driver.findElement(By.xpath("//div[@class='card']/h4[text()='Range Slider']"));
+		action.moveToElement(move).perform();
 
 		driver.close();
+
 	}
 
 }
