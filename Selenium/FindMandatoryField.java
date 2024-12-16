e75d77e06b18595c25da08f3b3d13d7f8c6aca0c
Kavya B <161735556+Kavyabala28@users.noreply.github.com>
2024-12-16 13:01:49 +0530

    Add files via upload

--- Selenium/FindMandatoryField.java
+++ Selenium/FindMandatoryField.java
@@ -11,6 +11,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindMandatoryField {
+	
+   /* Examples of pseudo-elements:
+   
+	::first-line
+	::selection
+	::before
+	::after
+	::has */
 
 	public static void main(String[] args) {
 
